88ee43b26ac4c2b77660b87b931f1eebc7ed14ca
trannguyen <tran.nguyen@colonialsurety.com>
2026-02-10 12:45:28 +0700

    refactor: removing default parameters

--- src/app/services/permission.ts
+++ src/app/services/permission.ts
@@ -9,45 +9,44 @@ export class PermissionService {
 
   currentUser = computed(() => this.userService.currentUser());
 
-  hasRole(requiredRoles: string[] = [], matchAll = true): boolean {
-    if (!this.currentUser()) {
+  hasRole(requiredRoles: string[], matchAll = true): boolean {
+    const currentUser = this.currentUser();
+    if (!currentUser) {
       return false;
     }
 
     if (matchAll) {
-      return requiredRoles.every((role) => this.currentUser()!.roles?.includes(role));
+      return requiredRoles.every((role) => currentUser.roles?.includes(role));
     }
 
-    return requiredRoles.some((role) => this.currentUser()!.roles?.includes(role));
+    return requiredRoles.some((role) => currentUser.roles?.includes(role));
   }
 
-  hasDepartment(requiredDepartments: string[] = [], matchAll = true): boolean {
-    if (!this.currentUser()) {
+  hasDepartment(requiredDepartments: string[], matchAll = true): boolean {
+    const currentUser = this.currentUser();
+    if (!currentUser) {
       return false;
     }
 
     if (matchAll) {
       return requiredDepartments.every((department) =>
-        this.currentUser()!.departments?.includes(department),
+        currentUser.departments?.includes(department),
       );
     }
-    return requiredDepartments.some((department) =>
-      this.currentUser()!.departments?.includes(department),
-    );
+    return requiredDepartments.some((department) => currentUser.departments?.includes(department));
   }
 
-  hasPermission(requiredPermissions: string[] = [], matchAll = true): boolean {
-    if (!this.currentUser()) {
+  hasPermission(requiredPermissions: string[], matchAll = true): boolean {
+    const currentUser = this.currentUser();
+    if (!currentUser) {
       return false;
     }
 
     if (matchAll) {
       return requiredPermissions.every((permission) =>
-        this.currentUser()!.permissions?.includes(permission),
+        currentUser.permissions?.includes(permission),
       );
     }
-    return requiredPermissions.every((permission) =>
-      this.currentUser()!.permissions?.includes(permission),
-    );
+    return requiredPermissions.every((permission) => currentUser.permissions?.includes(permission));
   }
 }
